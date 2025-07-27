5691c3e1e6e0f63f21385b20a9c14a71a0b51ece
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-07-28 00:07:31 +0300

    Parse user prompt and verbosity toggle as command line args

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    # Exit the program without prompt as command-line arg
+    if len(sys.argv) < 2:
+        print("Usage: uv run main.py <prompt> [--verbose]")
+        sys.exit(1)
+    
+    # Capture the user's prompt
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    
     # Read the gemini api key from the env variable
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,12 +27,18 @@ def main():
     # Prompt the client instance
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="How does the number 42 relate to Douglas Adam's Hitchhikers Guide to the Galaxy Sci-fi novel?"
+        contents=messages
     )            
     
-    print(f"{response.text}\n")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # Displaly optional content based on command-line arguments
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"{response.text}\n")
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(f"{response.text}")
 
 if __name__ == "__main__":
     main()
