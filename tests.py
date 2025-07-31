dcb3df169fd3dc21e9738cd18845d403a037d9ce
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-07-31 14:06:38 +0300

    Test the run_python_file function

--- tests.py
+++ tests.py
@@ -1,19 +1,21 @@
 # Import function
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 # Define the test cases 
 test_cases = [
-    ("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
-    ("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
-    ("calculator", "/tmp/temp.txt", "this should not be allowed")
+    ("calculator", "main.py"),
+    ("calculator", "main.py", ["3 + 5"]),
+    ("calculator", "tests.py"),
+    ("calculator", "../main.py"),
+    ("calculator", "nonexistent.py")
 ]
 
-def test(input1, input2, input3):
+def test(input1, input2, input3=[]):
     # Handle output for files
     print(f"Result for '{input2}' file:")
     
     # Handle the result output for valid and invalid inputs
-    result = write_file(input1, input2, input3)
+    result = run_python_file(input1, input2, input3)
     if result.startswith("Error"):
         print(f"\t{result}")
     else:
