c1f16e5d75f4208b9c212c1f1426768e98c3f0e0
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-08-01 19:42:12 +0300

    Add and test system prompt

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 MAX_CHARS=10000
+MODEL_NAME="gemini-2.0-flash-001"
+SYSTEM_PROMPT='Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import MODEL_NAME, SYSTEM_PROMPT
 
 
 def main():
@@ -26,8 +27,9 @@ def main():
 
     # Prompt the client instance
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages
+        model=MODEL_NAME,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT)
     )            
     
     # Displaly optional content based on command-line arguments
