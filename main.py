723af26144a9818b873a6bb8ff00ac93d06bab66
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-08-09 14:37:39 +0300

    Refactor code

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import *
-from call_function import available_functions
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.run_python import run_python_file
-from functions.write_file import write_file
+from call_function import available_functions, call_function
 
 def main():
     # Read the gemini api key from the env variable
@@ -34,17 +30,22 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}\n")
     
-    # Iteratively call the generate content function 20 times
-    try:
-        for i in range(20):
-            generated_response = generate_content(client, messages, verbose)
-            if generated_response:
-                print(generated_response)
-                break
-    except Exception as e:
-        print(f"Error: {e}")
+    # Allow model to iteratively call functions and cap the no. of iterations
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
         
-
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+        
 
 def generate_content(client, messages, verbose):
     # Prompt the client instance
@@ -58,80 +59,34 @@ def generate_content(client, messages, verbose):
     )
     
     # Add list of response variations to the messages
-    for candidate in response.candidates:
-        messages.append(candidate.content)
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
     
     # Return the LLM text response if no function was called
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:    
         function_call_result = call_function(function_call_part, verbose)
         
-        function_call_message = types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_call_part.name,
-                    response={"result":f"{function_call_result}"}
-                )  
-            ],
-        )
-        
-        messages.append(function_call_message)
-        
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception(f"error: {function_call_part.name} did not execute as intended.")
-    
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
         if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-
-def call_function(function_call_part, verbose=False):
+        function_responses.append(function_call_result.parts[0])
     
-    if verbose:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(f" - Calling function: {function_call_part.name}")
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
     
-    # Create dictionary mapping function names to function objects
-    function_map = {
-        "get_file_content": get_file_content,
-        "get_files_info": get_files_info,
-        "write_file": write_file,
-        "run_python_file": run_python_file,
-    }
-    
-    function_name = function_call_part.name
-    
-    if function_name not in function_map:
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_name,
-                    response={"error": f"Unknown function: {function_name}"},
-                )
-            ],
-        )
-    
-    if function_name in function_map:
-        function_call = function_map[function_name]
-        args_dict = function_call_part.args.copy()
-        args_dict["working_directory"] = "./calculator"
-        function_result = function_call(**args_dict)
-        
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_name,
-                    response={"result": function_result},
-                )
-            ],
-        )
+    messages.append(types.Content(role="tool", parts=function_responses))
    
     
 if __name__ == "__main__":
